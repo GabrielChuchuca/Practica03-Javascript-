8be96899419a1d23125cb8b701f9ed20e390ede2
GabrielChuchuca <gavriellalx@hotmail.com>
2019-05-22 03:37:46 -0500

    Actualizado

--- galeriaimagenes/js/funciones.js
+++ galeriaimagenes/js/funciones.js
@@ -17,12 +17,15 @@ function hola() {
     console.log(participantes)
     num = 0
     for (var i = 0; i < 5; i++) {
-        participantes[i] = Math.round(Math.random() * 9) + 1
+        participantes[i] = Math.floor(Math.random() * (1-(10+1))+(10))
+
+        //participantes[i] = Math.floor(Math.random() * (10))
     }
     console.log(participantes)
+    var par2 = participantes.slice(0, 5)
     console.log(num)
 
-    var direccion = "images/foto" + participantes[num] + ".jpg";
+    var direccion = "images/foto" + par2[num] + ".jpg";
     console.log(direccion);
     document.getElementById("foto").src = direccion;
 
@@ -104,8 +107,29 @@ function siguiente() {
 }
 
 function anterior() {
+    num--
+    //console.log(num)
+    if (num == 4) {
+        document.getElementById("next").disabled = true;
+        document.getElementById("previous").disabled = false;
 
-    var participant = ['images/foto1.jpg', 'images/foto2.jpg', 'images/foto3.jpg', 'images/foto4.jpg', 'images/foto5.jpg', 'images/foto6.jpg', 'images/foto7.jpg', 'images/foto8.jpg', 'images/foto9.jpg'];
+    }
+
+    else if (num == 0) {
+        document.getElementById("previous").disabled = true;
+        document.getElementById("next").disabled = false;
+
+    } else {
+        document.getElementById("next").disabled = false;
+        document.getElementById("previous").disabled = false;
+
+
+    }
+    direccion = "images/foto" + participantes[num] + ".jpg";
+    console.log(direccion);
+    document.getElementById("foto").src = direccion;
+
+    /*var participant = ['images/foto1.jpg', 'images/foto2.jpg', 'images/foto3.jpg', 'images/foto4.jpg', 'images/foto5.jpg', 'images/foto6.jpg', 'images/foto7.jpg', 'images/foto8.jpg', 'images/foto9.jpg'];
     var num_pa = participant.length;
     console.log(num_pa)
     numer = (Math.floor(Math.random() * num_pa))
